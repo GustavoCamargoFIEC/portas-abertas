37b2b604d424229a81de716d054ccc7d3683219a
Gustavo Henrique de Camargo <gustavo30899@fiec.edu.br>
2024-06-03 22:49:01 -0300

    medias

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,14 @@
 var slide1= document.getElementById("slide1").getAttribute("data-bs-slide-to")
 var slide2= document.getElementById("slide2").getAttribute("data-bs-slide-to")
 var slide3= document.getElementById("slide3").getAttribute("data-bs-slide-to")
-var slide4= document.getElementById("slide4").getAttribute("data-bs-slide-to")
 
 var pag1= document.getElementById("pag1")
 var pag2= document.getElementById("pag2")
 var pag3= document.getElementById("pag3")
-var pag4= document.getElementById("pag4")
+
+var sm1= document.getElementById("sm1")
+var sm2= document.getElementById("sm2")
+var sm3= document.getElementById("sm3")
 
 var botao= document.getElementById("buttonBlock")
 
@@ -17,7 +19,9 @@ function mudarBlock() {
         pag1.style.display= "block"
         pag2.style.display= "none"
         pag3.style.display= "none"
-        pag4.style.display= "none"
+        sm1.style.display= "block"
+        sm2.style.display= "none"
+        sm3.style.display= "none"
     }
     if (document.getElementById("slide2").classList== 'active') {
         botao.href= "templates/componentes.html"
@@ -25,7 +29,9 @@ function mudarBlock() {
         pag1.style.display= "none"
         pag2.style.display= "block"
         pag3.style.display= "none"
-        pag4.style.display= "none"
+        sm1.style.display= "none"
+        sm2.style.display= "block"
+        sm3.style.display= "none"
     }
     if (document.getElementById("slide3").classList== 'active') {
         botao.href= "templates/jogos.html"
@@ -33,14 +39,8 @@ function mudarBlock() {
         pag1.style.display= "none"
         pag2.style.display= "none"
         pag3.style.display= "block"
-        pag4.style.display= "none"
-    }
-    if (document.getElementById("slide4").classList== 'active') {
-        botao.href= "templates/formulario.html"
-        botao.setAttribute("target","_self")
-        pag1.style.display= "none"
-        pag2.style.display= "none"
-        pag3.style.display= "none"
-        pag4.style.display= "block"
+        sm1.style.display= "none"
+        sm2.style.display= "none"
+        sm3.style.display= "block"
     }
 }
